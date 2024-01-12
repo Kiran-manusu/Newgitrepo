02aab67a30c2b9307eff46883196987b6dbd3b80
gopikiranmanusu <manusu.gopikiran@serosoft.in>
2024-01-12 16:45:21 +0530

    kiran commit

--- src/test/java/gitdemo/Second.java
+++ src/test/java/gitdemo/Second.java
@@ -3,7 +3,7 @@ package gitdemo;
 public class Second {
 
 	public static void main(String[] args) {
-		System.out.println("Gopi new branch code");
+		System.out.println("Kiran new branch code");
 
 	}
 
